e567f31085ae6240be5242a2c5a031e5272321d5
ZahuseK <zahusek@gmail.com>
2015-07-19 22:52:39 +0200

    Update Main.java

--- example/tab/Main.java
+++ example/tab/Main.java
@@ -1,67 +1,39 @@
-package com.gmail.zahusek.tabtest;
+package com.gmail.zahusek.test;
 
-import java.util.Random;
+import java.util.stream.IntStream;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import com.gmail.zahusek.api.tabapi.TabAPI;
+import com.gmail.zahusek.tinyprotocolapi.api.tabapi.TabAPI;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		Bukkit.getPluginManager().registerEvents(this, this);
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				Bukkit.getOnlinePlayers().forEach((player) -> {
-					updateTab(player);
-				});
-			}
-		}.runTaskTimer(this, 20L, 5L);
-	}
-	public void updateTab(Player p){
-		
-		TabAPI.setTabString(p, 1, 1, "    " +toRainbow(p.getName()));
-		TabAPI.modifyTab(p);
-	}
-	public String $(String s){
-		return ChatColor.translateAlternateColorCodes('&', s);
-	}
-	
-	@EventHandler
-	public void join(PlayerJoinEvent e){
-		Player p = e.getPlayer();
 		new BukkitRunnable() {
 			
 			@Override
 			public void run() {
-				TabAPI.setTabSettings(p, 3, 20, 0);
-				
-				TabAPI.setTabString(p, 0, 0, $("&6o&eO&6o&eO&6o&eOO&6o&eO&6o&eO&6o"));
-				TabAPI.setTabString(p, 1, 0, $("&6o&eO&6o&eO&6o&eOO&6o&eO&6o&eO&6o"));
-				TabAPI.setTabString(p, 2, 0, $("&6o&eO&6o&eO&6o&eOO&6o&eO&6o&eO&6o"));
-				
-				TabAPI.setTabString(p, 0, 1, $("&6o&eO&6o&eO&6o&eOO&6o&eO&6o&eO&6o"));
-				TabAPI.setTabString(p, 2, 1, $("&6o&eO&6o&eO&6o&eOO&6o&eO&6o&eO&6o"));
-				
-				TabAPI.setTabString(p, 0, 2, $("&6o&eO&6o&eO&6o&eOO&6o&eO&6o&eO&6o"));
-				TabAPI.setTabString(p, 1, 2, $("&6o&eO&6o&eO&6o&eOO&6o&eO&6o&eO&6o"));
-				TabAPI.setTabString(p, 2, 2, $("&6o&eO&6o&eO&6o&eOO&6o&eO&6o&eO&6o"));
-				TabAPI.modifyTab(p);
+				for(Player player : Bukkit.getOnlinePlayers())
+					getTab(player);
 			}
-		}.runTaskLaterAsynchronously(this, 20L);
+		}.runTaskTimer(this, 0, 5);
 	}
-	private static final char[] COLORS = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-	Random rand = new Random();
-	public String toRainbow(String name) {
-		return ChatColor.translateAlternateColorCodes('&', "&" + COLORS[rand.nextInt(COLORS.length)] + name);
+	public void getTab(Player player) {
+		IntStream.range(0, 20).forEach((y) -> IntStream.range(0, 4).forEach((x) -> {
+			setTabSlot(player, x, y, ChatColor.translateAlternateColorCodes('&', "&8##########"));
+		}));
+		setTabSlot(player, 1, 1, ChatColor.translateAlternateColorCodes('&', "&6Wlecome"));
+		setTabSlot(player, 2, 1, ChatColor.translateAlternateColorCodes('&', "&a" + player.getName()));
+		TabAPI.setTabHnF(player, ChatColor.translateAlternateColorCodes('&', "&aHeader"), ChatColor.translateAlternateColorCodes('&', "&6Footer"));
+		TabAPI.modifyTab(player);
 	}
+	public void setTabSlot(Player player, int x, int y, String msg){
+		TabAPI.setTabSlot(player, x, y, msg, 0);
+	}
+
 }
