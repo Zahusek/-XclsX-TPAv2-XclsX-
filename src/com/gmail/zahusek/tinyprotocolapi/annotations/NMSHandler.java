e634e5b1bc30fe2bd83dd120780d98d4782b11ea
ZahuseK <zahusek@gmail.com>
2015-07-20 18:51:13 +0200

    NMS

---
+++ src/com/gmail/zahusek/tinyprotocolapi/annotations/NMSHandler.java
@@ -0,0 +1,14 @@
+package com.gmail.zahusek.tinyprotocolapi.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NMSHandler {
+
+	String name();
+
+}
